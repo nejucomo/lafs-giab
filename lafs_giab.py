e216f5dc65530443be62e8ad4d17a6e4d0531c3e
Nathan Wilcox <nejucomo@gmail.com>
2013-06-14 09:18:54 -0700

    Start "Grid In A Box" tool.

---
+++ lafs_giab.py
@@ -0,0 +1,58 @@
+#! /usr/bin/env python
+
+import os
+import sys
+import argparse
+import logging
+import subprocess
+
+
+DESCRIPTION = """
+Tahoe-LAFS Grid In A Box - create/configure/start/stop a self-contained set of nodes.
+"""
+
+
+def main(args = sys.argv[1:]):
+    opts = parse_args(args)
+    opts.run(opts)
+
+
+def parse_args(args):
+    ptop = argparse.ArgumentParser(description=DESCRIPTION)
+
+    ptop.add_argument('--base-dir',
+                      dest='basedir',
+                      type=str,
+                      default=os.path.expanduser('~/.lafs-giab'),
+                      help='The base directory which contains node directories.')
+
+    ptop.add_argument('--log-level',
+                      dest='loglevel',
+                      #default='INFO',
+                      default='DEBUG',
+                      choices=['DEBUG', 'INFO', 'WARN', 'ERROR', 'CRITICAL'],
+                      help='Set logging level.')
+
+    subps = ptop.add_subparsers(title='Operations')
+    subp = subps.add_parser('create', help='Create multiple nodes at once.')
+    subp.set_defaults(run=command_create)
+
+    opts = ptop.parse_args(args)
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        format='%(asctime)s %(levelname) 5s %(name)s | %(message)s',
+        datefmt='%Y-%m-%dT%H:%M:%S%z',
+        level=getattr(logging, opts.loglevel))
+
+    return opts
+
+
+def command_create(opts):
+    log = logging.getLogger('create')
+    log.debug('create --base-dir %r' % (opts.basedir,))
+    raise NotImplemented('create')
+
+
+if __name__ == '__main__':
+    main()
