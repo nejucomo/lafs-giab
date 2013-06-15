d7c1ae857cdb2c068b89d184ed1e1fc3e7741907
Nathan Wilcox <nejucomo@gmail.com>
2013-06-15 08:00:13 -0700

    Much structure to deal with commandline parsing; partially-implemented launch command; implemented start/stop/restart delegations.

--- lafs_giab.py
+++ lafs_giab.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 
-import os
-import sys
 import argparse
+import errno
 import logging
+import os
 import subprocess
+import sys
+import time
 
 
 DESCRIPTION = """
@@ -14,16 +16,22 @@ Tahoe-LAFS Grid In A Box - create/configure/start/stop a self-contained set of n
 
 def main(args = sys.argv[1:]):
     opts = parse_args(args)
-    opts.run(opts)
+    paths = NodePaths(opts.basedir)
+    opts.run(opts, paths)
+
+
+# This global is populated at module load time below:
+CommandTable = {}
 
 
 def parse_args(args):
     ptop = argparse.ArgumentParser(description=DESCRIPTION)
 
-    ptop.add_argument('--base-dir',
+    ptop.add_argument('--dir', '-d',
                       dest='basedir',
                       type=str,
-                      default=os.path.expanduser('~/.lafs-giab'),
+                      required=True,
+                      #default=os.path.expanduser('~/.lafs-giab'),
                       help='The base directory which contains node directories.')
 
     ptop.add_argument('--log-level',
@@ -34,8 +42,10 @@ def parse_args(args):
                       help='Set logging level.')
 
     subps = ptop.add_subparsers(title='Operations')
-    subp = subps.add_parser('create', help='Create multiple nodes at once.')
-    subp.set_defaults(run=command_create)
+
+    for cmdname, cmdfunc in sorted(CommandTable.items()):
+        subp = subps.add_parser(cmdname, help=cmdfunc.__doc__)
+        subp.set_defaults(run=cmdfunc)
 
     opts = ptop.parse_args(args)
 
@@ -53,8 +63,117 @@ def parse_args(args):
     return opts
 
 
-def command_create(opts):
-    raise NotImplementedError('create')
+
+class NodePaths (object):
+
+    Names = ['introducer', 'node']
+
+    def __init__(self, basedir):
+        for name in self.Names:
+            setattr(self, name, os.path.join(basedir, name))
+
+
+def register_command(f, name=None):
+    """Add command function f to CommandTable; may be used as a decorator."""
+    if name is None:
+        name = f.__name__
+
+    def log_wrapper(*args):
+        log = logging.getLogger(name)
+        return f(log, *args)
+
+    log_wrapper.__doc__ = f.__doc__
+
+    CommandTable[name] = log_wrapper
+
+    return log_wrapper
+
+
+@register_command
+def launch(log, opts, paths):
+    """Start all nodes, creating and configuring each from scratch if necessary."""
+
+    makedir_if_necessary(opts.basedir)
+    if makedir_if_necessary(paths.introducer):
+        log.info('Creating introducer')
+        run('tahoe', 'create-introducer', paths.introducer)
+    log.info('Starting introducer')
+    run('tahoe', 'start', '--basedir', paths.introducer)
+
+    introfurlpath = os.path.join(paths.introducer, 'private', 'introducer.furl')
+
+    opened = False
+    while not opened:
+        log.debug('Waiting for %r', introfurlpath)
+        time.sleep(1)
+        (opened, f) = std_try(errno.ENOENT, open, introfurlpath, 'rb')
+
+    with f:
+        introfurl = f.read()
+
+    log.info('Found introducer.furl: %r', introfurl)
+
+    raise NotImplementedError()
+
+
+
+def register_standard_dispatch_command(commandname):
+
+    def cmd_impl(log, opts, paths):
+
+        for name in paths.Names:
+            path = getattr(paths, name)
+
+            log.info('%s %s', name, commandname)
+            run('tahoe', commandname, '--basedir', path)
+
+    cmd_impl.__doc__ = 'Run "tahoe %s" on each node directory.' % (commandname,)
+
+    register_command(cmd_impl, commandname)
+
+
+for cmdname in ['start', 'stop', 'restart']:
+    register_standard_dispatch_command(cmdname)
+
+del cmdname
+
+
+
+def run(command, *args):
+    argv = (command,) + args
+
+    log = logging.getLogger('command %r' % (command,))
+    log.debug('Running: %r', argv) # Wishlit: escape for /bin/sh cut'n'paste compatibility.
+
+    proc = subprocess.Popen(args=argv)
+    log.debug('PID: %r', proc.pid)
+
+    status = proc.wait()
+    log.debug('Exit Status: %r', status)
+
+    if status != 0:
+        raise SystemExit(status)
+
+
+
+def makedir_if_necessary(path):
+    (new, _) = std_try(errno.EEXIST, os.mkdir, path)
+    if new:
+        logging.debug('Created directory: %r', path)
+    return new
+
+
+
+def std_try(errcode, f, *args):
+    try:
+        result = f(*args)
+    except os.error, e:
+        if e.errno == errcode:
+            return (False, None)
+        else:
+            raise
+    return (True, result)
+
 
 
 if __name__ == '__main__':
